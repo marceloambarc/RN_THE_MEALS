61b2d04fc89c6f91330f06cf1a1bb4ef3c5ddcf0
marceloambarc <marcelo.ambarc@gmail.com>
2021-05-03 20:55:53 -0300

    Screens

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Touchable } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+const CategoriesScreen = props => {
+  const renderItemGrid = (itemData) => {
+    return (
+      <CategoryGridTile 
+        title={itemData.item.title} 
+        color={itemData.item.color}
+        onSelect={() => {
+          props.navigation.navigate({
+            routeName: 'CategoryMeals', 
+            params: {
+              categoryId: itemData.item.id
+            }
+          })
+        }} 
+      />
+    )
+  }
+
+  return (
+    <FlatList 
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderItemGrid}
+      numColumns={2}
+    />
+  );
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories'
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+export default CategoriesScreen;
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import MealList from '../components/MealList';
+import { MEALS }  from '../data/dummy-data';
+
+const FavoritesScreen = props => {
+  const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+  return (
+    <MealList listData={favMeals} navigation={props.navigation} />
+  );
+};
+
+FavoritesScreen.navigationOptions = {
+  headerTitle: 'Your Favorites'
+};
+
+export default FavoritesScreen;
